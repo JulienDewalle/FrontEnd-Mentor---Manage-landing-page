cad6f198a7ca00a40ab89d1c8732316a90bbc4db
Julien Dewalle <julien.dewalle@hotmail.fr>
2020-06-01 13:01:21 +0200

    menu burger responsive JS

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ document.querySelector("#toggle_btn").onclick = function() {
     if (window.getComputedStyle(document.querySelector('#nav')).display=='none'){
     document.querySelector("#nav").style.display="block";
     document.querySelector("body").style.backgroundColor="rgba(38, 12, 12, 0.30)";
-    } else {
+    } 
+    else 
+    {
     document.querySelector("#nav").style.display="none";
     document.querySelector("body").style.backgroundColor="#ffffff";
     }
 }
+
+myImage = document.querySelector('#toggle_btn');
+var actionImage = function () {
+    let getSrc = myImage.getAttribute('src')
+    if (getSrc === 'img/icon-hamburger.svg') {
+        myImage.setAttribute('src', 'img/icon-close.svg')
+    } else if (getSrc === 'img/icon-close.svg') {
+        myImage.setAttribute('src', 'img/icon-hamburger.svg')
+    }
+};
+myImage.addEventListener('click', actionImage);
